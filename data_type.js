96db433aa26c1fe04d5f4f81633a47d872a73158
Sagar Jadhav <93977940+sagarxjadhav@users.noreply.github.com>
2024-02-05 03:46:31 +0530

    2 new files
    my notes

---
+++ data_type.js
@@ -0,0 +1,31 @@
+// // Day 2
+
+// "use strict"; // treat all JS code as newer version
+
+// console.log(6+6)
+// console.log("sagar");
+
+// // Data Types
+
+// let name = "sagar"  // string 
+// let age = 21    // Number
+// let isMarried = true   // Boolean
+// let x = null     // Null
+// let undefinedVar  // Undefined
+// let obj = {name: 'Sagar', age: 21}  // Object
+
+// let arr = [1,2,3]      // Array
+
+// console.log(typeof 21); //Number
+// console.log(typeof undefined);  // undefined
+// console.log(typeof null);   // object
+
+let age="23";
+
+console.log(typeof age);
+
+let proage=Number(age);
+console.log(proage);
+console.log(typeof proage);
+
+
---
+++ one.js
@@ -0,0 +1,21 @@
+const accountId = 123456789;
+let accountEmail = "sagarxjadhav@google.com"
+var accountPassword = "12345"
+ accountCity = "Jaipur"
+ let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional Scope
+*/
+
+/*
+Variables and Data Types
+const = can't be re-assigned a value and can't be declared again  (block scope)
+let = can be re-assigned a value but can't be declared again (block scope)
+var = it can be re-assigned a value and it can also be declared again  (Global scope)
+*/
+
+
